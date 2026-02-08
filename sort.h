099df27ac72c5c99a37eb9d9f066741acfed1fc3
wydnickk <nnamazova374@gmail.com>
2026-02-08 12:01:42 +0000

    Add bubble sort in Betty style

--- sort.h
+++ sort.h
@@ -3,6 +3,13 @@
 
 #include <stddef.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -10,10 +17,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
 
